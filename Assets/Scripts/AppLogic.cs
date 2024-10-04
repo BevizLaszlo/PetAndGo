c81de8655aa732f9b072ba8fa9a2c288e2462915
Csizmadia Márk <airamek@teamorange.hu>
2024-10-04 18:54:52 +0200

    Kalkulátor kezdetek

---
+++ Assets/Scripts/AppLogic.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppLogic : MonoBehaviour
+{
+    public static int VisszavaltasiAr = 50; // HUF
+
+    public static List<Visszavalthato> visszavalthatok;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static void Add()
+    {
+        
+    }
+}
