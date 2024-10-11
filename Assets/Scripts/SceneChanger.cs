ee0f9ae9c8210a5e878fbe4a13844eb5a596a1c6
Bevíz László <bevizlaci@gmail.com>
2024-10-11 19:10:20 +0200

    update

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -1,22 +1,30 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
     public Animator transition;
+
     public void MoveToScene(string sceneName)
     {
-        StartCoroutine(LoadLevel(sceneName).GetEnumerator());
+        if (!this.gameObject.activeSelf)
+        {
+            this.gameObject.SetActive(true);
+        }
+        // Start the coroutine properly
+        StartCoroutine(LoadLevel(sceneName));
     }
 
-    IEnumerable LoadLevel(string sceneName)
+    IEnumerator LoadLevel(string sceneName)
     {
+        // Start animation
         transition.SetTrigger("Start");
 
+        // Wait for the animation duration (adjust the duration based on your animation)
         yield return new WaitForSeconds(1.5f);
 
+        // Load the scene
         SceneManager.LoadScene(sceneName);
     }
 }
